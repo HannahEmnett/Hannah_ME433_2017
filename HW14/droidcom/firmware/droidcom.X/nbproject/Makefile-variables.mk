3a02fcd0e1a917af15ceb9379e2597f78c9642a4
HannahEmnett <hannahemnett@gmail.com>
2017-05-14 16:17:28 -0500

    did the usb phone thing

---
+++ HW14/droidcom/firmware/droidcom.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=droidcom.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/droidcom.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=droidcom.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/droidcom.x.tar
